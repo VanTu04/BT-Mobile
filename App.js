c49b80defac87097813bb82703f6c805904e73f0
VanTu04 <141947683+VanTu04@users.noreply.github.com>
2025-01-07 15:50:33 +0700

    bt2-2

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import Square from './Components/Square';
+import RenderItem from './Components/RenderItem';
 
 export default function App() {
+  const data = [
+    {id: "1", header: "Bạn có khách hàng mới!", title: "Chúc mừng bạn, bạn có khách hàng mới.Hãy mau chóng liên lạc.", time: "20/08/2020, 09:20"},
+    {id: "2", header: "Bạn có khách hàng mới!", title: "Chúc mừng bạn, bạn có khách hàng mới.Hãy mau chóng liên lạc.", time: "20/08/2020, 09:20"},
+    {id: "3", header: "Bạn có khách hàng mới!", title: "Chúc mừng bạn, bạn có khách hàng mới.Hãy mau chóng liên lạc.", time: "20/08/2020, 09:20"},
+    {id: "4", header: "Bạn có khách hàng mới!", title: "Chúc mừng bạn, bạn có khách hàng mới.Hãy mau chóng liên lạc.", time: "20/08/2020, 09:20"},
+    {id: "5", header: "Bạn có khách hàng mới!", title: "Chúc mừng bạn, bạn có khách hàng mới.Hãy mau chóng liên lạc.", time: "20/08/2020, 09:20"},
+  ]
   return (
-    <View style={styles.container}>
-      <Square title="Hello, world" />
-      {/* <Text>Hello</Text> */}
-    </View>
+    <SafeAreaView style={styles.container}>
+      {/* <Square title="Hello, world" /> */}
+      <FlatList 
+        data={data}
+        renderItem={
+          ({item}) => <RenderItem header={item.header} title={item.title} time={item.time} />
+        }
+        keyExtractor={(item) => item.id}
+      />
+    </SafeAreaView>
   );
 }
 
@@ -14,6 +28,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
+    backgroundColor: 'rgb(181,238,191)',
     justifyContent: 'center',
   },
 });
--- Readme.md
+++ Readme.md
@@ -1,2 +1,2 @@
 ## Nguyễn Văn Tú - 22810310083
-![bt2.1](./assets/bt2-1.jpg)
+![bt2.1](./assets/bt2-2.jpg)
